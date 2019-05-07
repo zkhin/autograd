a66e7d13de12ff68acf2411b1efe509f1d5cdf51
zkhin <arandomname@gmail.com>
2019-05-06 21:39:44 -0400

    Update tensor.py

--- tensor.py
+++ tensor.py
@@ -1,5 +1,5 @@
 """
-Simple extensible implementation of automatic gradients in a TensorFlow-like library.
+Simple extensible implementation of automatic gradients in a Torch-like library.
 Part of work to understand Torch library to decouple and simplfy components to use in other languages.
 """
 
